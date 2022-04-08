620c0fd91e56af9a3bfa3cbf1f91e84e125c9339
root <hassan_salam>
2022-04-08 04:47:18 -0700

    printing alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -To print lower alphabet
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
+
