c6026fc1bd7a282f6f99d4c4223c3cca1e194707
root <hassan_salam>
2022-04-12 07:35:28 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-ptchar.c
@@ -0,0 +1,39 @@
+/*
+ *
+ *  * file -1-alphabet.c
+ *
+ *   */
+
+
+
+#include "main.h"
+
+
+
+/**
+ *
+ *  * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ *
+ *   */
+
+
+
+void print_alphabet(void)
+
+{
+
+
+
+		char letter;
+
+
+
+			for (letter = 'a'; letter <= 'z'; letter++)
+
+i						_putchar(letter);
+
+
+
+				_putchar('\n');
+
+}
